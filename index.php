4d29c22552f09f3c006a60ced2b2b4bc676e8792
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-05-24 16:36:34 -0500

    feat: :zap: 1 POINT
    Construir el algoritmo para un programa que ingrese tres notas de un alumno, si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, de lo contrario un mensaje que diga "becado"

--- index.php
+++ index.php
@@ -12,20 +12,23 @@
 <body>
     <div class="div">
         <form action="index.php" method="post">
-            <h1>NUMBER</h1>
-            <input type="number" class="boot" name="num"><br><br>
+            <h1>NOTES</h1>
+            <input type="number" step="0.1" class="boot" name="one"><br>
+            <input type="number" step="0.1" class="boot" name="two"><br>
+            <input type="number" step="0.1" class="boot" name="three"><br>
             <button type="submit" class="boot">CHECK</button>
         </form>
     </div>
     <div class="div">
         <?php
         if ($_POST) {
-            $num = (integer)"{$_POST["num"]}";
-            
-            include("validationImput.php");
-        };
+            $one = (float) "{$_POST["one"]}";
+            $two = (float) "{$_POST["two"]}";
+            $three = (float) "{$_POST["three"]}";
 
-            ?>
+            include("validationImput.php");
+        }
+        ?>
     </div>
     </div>
 </body>
